a91762bfb99dc5783fdd4d566e1dd57997598805
marts2007 <admin.marts@gmail.com>
2021-07-29 19:26:57 +0300

    Update binding-hack.sh

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tone_analyzer="<you tone analyzer service name>"
+tone_analyzer="tone-analyzer-svc"
 
 B64_URL=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.url -j | base64 -w 0)
 B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
@@ -10,7 +10,7 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: binding-tone
-  namespace: <my_project>
+  namespace: sn-labs-marts2007
 type: Opaque
 data:
   url: $B64_URL
